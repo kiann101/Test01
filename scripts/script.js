58c5b3d99444c84da341b1d01e3823d61b871d3b
kiann <kiann0901@gmail.com>
2024-09-05 14:05:43 +0500

    add files

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+
+const links = document.querySelectorAll(".hero-down");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Menu
+
+function openModal() {
+  document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.top = "-500px";
+}
